f6a89ca117391ad8559d52a8164c9cc505704afc
RushawnCampbell <shoutme.sean@gmail.com>
2022-03-30 21:41:36 -0500

    Created first component

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,14 +1,37 @@
 <template>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
+ 
+ <div id="articlesparent">
+    <div v-for= "article in articles" class="card" style="width: 18rem;">
+        <img class="card-img-top" v-bind:src=article.urlToImage alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">{{ article.title }}</h5>
+            <p class="card-text">{{article.description}}</p>
+        </div>
+    </div>
+</div>
 </template>
 <script>
 export default {
  data() {
- return {};
+ return {
+    articles: []
+ };
  },
+ created() {
+     let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us',
+    {
+        headers: {
+            'Authorization': `Bearer 85c0b0940af649688c9dc2455f8ee68e`
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+    });
+}
 };
 </script>
