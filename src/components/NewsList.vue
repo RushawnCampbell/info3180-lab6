498b01d2f6758c5d652c49ca2cf041a20642d41f
RushawnCampbell <shoutme.sean@gmail.com>
2022-03-30 21:58:40 -0500

    Added search form and bindings

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,8 +1,17 @@
 <template>
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+        <label class="visually-hidden" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm"
+        id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        <p>You are searching for {{ searchTerm }}</p>
+    </form>
  
- <div id="articlesparent">
+<div id="articlesparent">
     <div v-for= "article in articles" class="card" style="width: 18rem;">
-        <img class="card-img-top" v-bind:src=article.urlToImage alt="Card image cap">
+        <img class="card-img-top" v-bind:src=article.urlToImage alt="No image available">
         <div class="card-body">
             <h5 class="card-title">{{ article.title }}</h5>
             <p class="card-text">{{article.description}}</p>
@@ -14,8 +23,27 @@
 export default {
  data() {
  return {
-    articles: []
+    articles: [],
+    searchTerm: ''
  };
+ },
+ methods: {
+ searchNews() {
+    let self = this;
+        fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en', {
+        headers: {
+        'Authorization': `Bearer 85c0b0940af649688c9dc2455f8ee68e`,
+        }
+        })
+        .then(function(response) {
+        return response.json();
+        
+        })
+        .then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+        });
+    }
  },
  created() {
      let self = this;
